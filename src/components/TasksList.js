f53f71e9e6e4190df4a4f1e79cfd0d277ea18398
gsak3l <gsakel5@gmail.com>
2020-08-16 09:46:28 +0300

    things added

---
+++ src/components/TasksList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TasksList = () => {
+  return <div>TaskList Component</div>;
+};
+
+export default TasksList;
---
+++ src/components/TasksPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TasksPage = () => {
+  return (
+    <div className="container my-5">
+      <div className="jumbotron py-3">
+        <div className="row">
+          <div className="col-md-2">
+            <button className="btn btn-success m-3"></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TasksPage;
