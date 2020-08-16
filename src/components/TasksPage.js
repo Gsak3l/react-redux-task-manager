6bbca7a33666bccdf8eafc1b9bbdada8e92e4373
gsak3l <gsakel5@gmail.com>
2020-08-16 10:00:08 +0300

    form style added

--- src/components/TasksPage.js
+++ src/components/TasksPage.js
@@ -1,4 +1,6 @@
 import React from "react";
+// Material Icons
+import AddIcon from "@material-ui/icons/Add";
 
 const TasksPage = () => {
   return (
@@ -6,8 +8,35 @@ const TasksPage = () => {
       <div className="jumbotron py-3">
         <div className="row">
           <div className="col-md-2">
-            <button className="btn btn-success m-3"></button>
+            <button className="btn btn-success m-3">
+              <AddIcon />
+            </button>
           </div>
+          <div className="col-md-10">
+            <h2 className="display-4 text-center text-uppercase">
+              Task Manager
+            </h2>
+          </div>
+          {/* Inputs Form*/}
+          <form>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Task Title"
+              />
+            </div>
+            <div className="form-group">
+              <textarea
+                type="text"
+                className="form-control"
+                placeholder="Task Description"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </div>
