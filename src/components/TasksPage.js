49714c2cd1301893370f7d546fd3e273454d031c
gsak3l <gsakel5@gmail.com>
2020-08-16 12:29:58 +0300

    TasksPage UI finished

--- src/components/TasksPage.js
+++ src/components/TasksPage.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { useState } from "react";
 // Material Icons
 import AddIcon from "@material-ui/icons/Add";
 
 const TasksPage = () => {
+  const [cardForm, showCardForm] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const formToggler = () => {
+    showCardForm(!cardForm);
+  };
+
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
   return (
     <div className="container my-5">
       <div className="jumbotron py-3">
         <div className="row">
           <div className="col-md-2">
-            <button className="btn btn-success m-3">
+            <button className="btn btn-success m-3" onClick={formToggler}>
               <AddIcon />
             </button>
           </div>
@@ -17,26 +34,31 @@ const TasksPage = () => {
               Task Manager
             </h2>
           </div>
-          {/* Inputs Form*/}
-          <form>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Task Title"
-              />
+          {cardForm && (
+            <div className="container">
+              <form>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Task Title"
+                    onChange={onChangeTitle}
+                  />
+                </div>
+                <div className="form-group">
+                  <textarea
+                    type="text"
+                    className="form-control"
+                    placeholder="Task Description"
+                    onChange={onDescriptionChange}
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </form>
             </div>
-            <div className="form-group">
-              <textarea
-                type="text"
-                className="form-control"
-                placeholder="Task Description"
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
+          )}
         </div>
       </div>
     </div>
